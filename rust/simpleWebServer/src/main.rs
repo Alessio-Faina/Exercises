e11088a839dc1d0a12a89936e3ccab6085ac2152
AlexF <AlexF@ita>
2025-04-30 21:15:45 +0200

    Changing a little bit the architecture, moving the logic out of main

--- rust/simpleWebServer/src/main.rs
+++ rust/simpleWebServer/src/main.rs
@@ -3,27 +3,24 @@ use std::{
 };
 
 mod http;
-use http::{constants::HttpResponseStatus, http_parser};
-use route::route::{call_route_by_error_code, call_route_by_path};
+use http::constants::HttpResponseStatus;
 mod route;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    let mut routes : Vec<route::route::Route> = vec![];
-    routes.push(route::route::add_route_by_path("/".to_string(),
-                route::index::get_response));
-    routes.push(route::route::add_route_by_error_code(HttpResponseStatus::NotFound,
-                route::not_found::get_response));
+    route::route::add_route_by_path("/".to_string(), route::index::get_response);
+    route::route::add_route_by_error_code(HttpResponseStatus::NotFound,
+                                        route::not_found::get_response);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream, &routes);
+        handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream, routes : &Vec<route::route::Route>) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -34,26 +31,7 @@ fn handle_connection(mut stream: TcpStream, routes : &Vec<route::route::Route>) 
     println!("Request: {http_request:#?}");
 
     #[allow(unused_assignments)]
-    let mut response: String= "".to_string();
-
-    match http::http_parser::parse_for_errors(&http_request) {
-        HttpResponseStatus::Ok => { 
-            let route = http_parser::get_route(&http_request);
-            let call_result: Result<String, route::route::RouteResolverError> = call_route_by_path(route, routes, &http_request);
-            match call_result {
-                Ok(res) => { response += &res },
-                Err(_) => { response += "INTERNAL ERROR"}
-            }
-        }
-        HttpResponseStatus::NotFound => { 
-            let call_result: Result<String, route::route::RouteResolverError> = 
-                    call_route_by_error_code(HttpResponseStatus::NotFound, routes, &http_request);
-            match call_result {
-                Ok(res) => {response += &res },
-                Err(_) => { response += "INTERNAL ERROR" }
-            }
-        }
-    } 
+    let mut response: String= route::route::call_route(&http_request);
 
     response += "\r\n\r\n";
     stream.write_all(response.as_bytes()).unwrap();
